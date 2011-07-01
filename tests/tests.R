d104b7741b1b87599d8d397e472374cd00e04c26
Jason Law <jason.e.law@gmail.com>
2011-07-01 13:41:16 -0700

    reorganize unit test code directories so they don't get deleted on install

--- tests/tests.R
+++ tests/tests.R
@@ -1,11 +1,12 @@
 library('RUnit')
 library(ORDEQWaterQualityCriteria)
 
-path <- file.path(.path.package(package="ORDEQWaterQualityCriteria"), "unitTests")
+path <- file.path(.path.package(package="ORDEQWaterQualityCriteria"), 
+                  "inst", "unitTests")
 test.suite <- defineTestSuite("ORDEQWaterQualityCriteria",
                               dirs = path,
                               testFileRegexp = "^test.+")
  
 test.result <- runTestSuite(test.suite)
-
+sink('.Rout.save')
 printTextProtocol(test.result)
