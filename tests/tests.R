7298beaf495564fb55b26aea21332dce64ce738d
Jason Law <jason.e.law@gmail.com>
2011-07-05 11:50:01 -0700

    fixed location of check.csv

--- tests/tests.R
+++ tests/tests.R
@@ -2,11 +2,12 @@ library('RUnit')
 library(ORDEQWaterQualityCriteria)
 
 path <- file.path(.path.package(package="ORDEQWaterQualityCriteria"), 
-                  "inst", "unitTests")
+                  "unitTests")
 test.suite <- defineTestSuite("ORDEQWaterQualityCriteria",
                               dirs = path,
                               testFileRegexp = "^test.+")
  
 test.result <- runTestSuite(test.suite)
+setwd('S:/Jlaw/ORDEQWaterQualityCriteria')
 sink('.Rout.save')
 printTextProtocol(test.result)
